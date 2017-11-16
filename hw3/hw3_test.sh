47f877e96c2a3a8c65d7914e5ecca3b92840752b
Itachi6912110 <itachi691211011129@gmail.com>
2017-11-16 20:31:10 +0800

    change test to wget

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -2,4 +2,6 @@
 
 tar zxvf test_models_1.tar.gz
 wget -O restruct_mdl7.h5  https://www.dropbox.com/s/qiojb75ovt4g5fp/restruct_mdl7.h5?dl=0
+wget -O model_sample_3.h5 https://www.dropbox.com/s/rnr3opxmyilnloa/model_sample_3.h5?dl=0
+wget -O model_sample_4.h5 https://www.dropbox.com/s/l67xd3qhb8mkhn3/model_sample_4.h5?dl=0
 python3 compare_model.py $1 $2
